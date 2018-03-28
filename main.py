88c8bebb64f773aeb36fa99f4497ab27ed20a9eb
MOAIZ AHMAD <S331389056@OCDSB>
2018-03-28 11:05:25 -0400

    .

--- main.py
+++ main.py
@@ -13,20 +13,30 @@ def FtoC (F):
     C = (0.55556)*(F-32)
     return C
 
-print('Enter 1 for celsius to fahrenheit or 2 for fahrenheit to celsius and them enter your number')
-choice = int(input())
-temperature = int(input())
-if choice == 1:
-    if temperature <= -273.15:
-        print('please enter a value greater')
-    else:
-        temperature = CtoF(temperature)
-        print(round(temperature))  
-elif choice == 2:
-    if temperature <= -459.67:
-        print('please enter a value greater')
-    else:
-        temperature = FtoC(temperature)
-        print(round(temperature))
-else:
-    print('please enter 1 or 2')
+ans = True
+while ans == True:
+    print('Enter 1 for celsius to fahrenheit or 2 for fahrenheit to celsius and them enter your number')
+    choice = int(input())
+    temperature = int(input())
+    if choice == 1:
+        if temperature <= -273.15:
+            print('please enter a value greater')
+        else:
+            temperature = CtoF(temperature)
+            print(round(temperature))  
+    elif choice == 2:
+        if temperature <= -459.67:
+            print('please enter a value greater')
+        else:
+            temperature = FtoC(temperature)
+            print(round(temperature))
+    elif choice != 1 or choice != 2:
+        print('please enter 1 or 2')
+    print('New Conversion? Y/N')
+    answ = str(input())
+    if answ == str('Y'):
+        print('Ok')
+    elif answ == str('N'):
+        print('Well then')
+        ans = False
+    
