61637285b5ebcf709a8319c33e0ba8ddb0fcd646
MOAIZ AHMAD <s331389056@ocdsb>
2018-03-28 10:58:44 -0400

    .

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# <Moaiz Ahmad>
 # March 28, 2018
 
 ###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF (C):
+    F=(1.8)*C+32
+    return F
 
 ###### uncomment this when you are ready to work on it
-#def FtoC ():
-#
+def FtoC (F):
+    C = (0.55556)*(F-32)
+    return C
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+print('Enter 1 for celsius to fahrenheit or 2 for fahrenheit to celsius and them enter your number')
+choice = int(input())
+temperature = int(input())
+if choice == 1:
+    if temperature <= -273.15:
+        print('please enter a value greater')
+    else:
+        temperature = CtoF(temperature)
+        print(round(temperature))  
+elif choice == 2:
+    if temperature <= -459.67:
+        print('please enter a value greater')
+    else:
+        temperature = FtoC(temperature)
+        print(round(temperature))
+else:
+    print('please enter 1 or 2')
